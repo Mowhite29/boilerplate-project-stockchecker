b3b3512f8a6ddb74e20b28749f0dd8c916967c85
Moses <mowhite29@hotmail.co.uk>
2025-01-31 17:53:13 +0000

    1

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,12 @@
 'use strict';
 const requestIp = require('request-ip');
 
-const SymbolDB = require('../components/database');
-const stockAPI = require('../components/stock-api');
+const SymbolDB = require('../components/database.js');
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      console.log(req.query.stock)
-      console.log(req.query.like)
       if (req.query.stock.length == 2){ //Pair of stock symbols
         const symbol0 = req.query.stock[0];
         const symbol1 = req.query.stock[1];
@@ -17,9 +14,14 @@ module.exports = function (app) {
         const symbol = req.query.stock;
         if (req.query.like){
           const clientIp = requestIp.getClientIp(req);
-          stockAPI(symbol).then(response => {
-            console.log(response)
-          })
+          let url = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/' + symbol + '/quote';
+          process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+          fetch(url)
+            .then(response =>response.json())
+            .then(response => {
+            console.log(response.symbol, response.latestPrice)
+            })
+          
         }else {
           
         }
